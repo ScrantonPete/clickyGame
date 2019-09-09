c20b2a456971fd9dbf5f2a34c7a67ab176743304
ScrantonPete <scrantonpete@yahoo.com>
2019-09-09 19:57:31 -0400

    Updates

---
+++ precache-manifest.bed3e2f0a44791b8b8b433e5d82b1525.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c92b797a93c4c86b5149f43bb8cf22",
+    "url": "/ScrantonPete/clickyGame/index.html"
+  },
+  {
+    "revision": "bb7ba6386473d809fb77",
+    "url": "/ScrantonPete/clickyGame/static/css/main.e949839c.chunk.css"
+  },
+  {
+    "revision": "b01c6ae6d5ba208cd079",
+    "url": "/ScrantonPete/clickyGame/static/js/2.2dd37698.chunk.js"
+  },
+  {
+    "revision": "bb7ba6386473d809fb77",
+    "url": "/ScrantonPete/clickyGame/static/js/main.b2327c21.chunk.js"
+  },
+  {
+    "revision": "445c44dda1633fbaf7e1",
+    "url": "/ScrantonPete/clickyGame/static/js/runtime~main.fb8d2443.js"
+  }
+]);
