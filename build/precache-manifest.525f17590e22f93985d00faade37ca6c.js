faf3c211f42ef06d56a031454c471efddb899a1b
ScrantonPete <scrantonpete@yahoo.com>
2019-09-09 19:38:00 -0400

    removed build folder from gitignore

---
+++ build/precache-manifest.525f17590e22f93985d00faade37ca6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2dcec092d1e31c81af22bdc33bd821",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "d874fe0d048ebe0e673d",
+    "url": "/clickyGame/static/css/main.dd9ebe5c.chunk.css"
+  },
+  {
+    "revision": "b01c6ae6d5ba208cd079",
+    "url": "/clickyGame/static/js/2.2dd37698.chunk.js"
+  },
+  {
+    "revision": "d874fe0d048ebe0e673d",
+    "url": "/clickyGame/static/js/main.f21ec491.chunk.js"
+  },
+  {
+    "revision": "afa8e8269c2cf6c21c7b",
+    "url": "/clickyGame/static/js/runtime~main.b1556a6a.js"
+  }
+]);
